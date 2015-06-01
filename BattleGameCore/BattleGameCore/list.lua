d08bf7efc4d2313da0e2ca6d7f54d22337e78cdb
lkstc112233 <296105011@qq.com>
2015-06-01 11:08:38 +0800

    list commit
    That was a miss :)

--- BattleGameCore/BattleGameCore/list.lua
+++ BattleGameCore/BattleGameCore/list.lua
@@ -2,7 +2,6 @@ list = {}
 
 list.head = 0
 list.tail = 0
-list.store = {}
 
 function list.new()
 	local ins = {}
@@ -14,7 +13,7 @@ function list.new()
 end
 
 function list.inelm(self,elm)
-	self.store[self.head] = elm
+	self[self.head] = elm
 	self.head = self.head + 1
 end
 
@@ -22,13 +21,13 @@ function list.outelm(self)
 	if self.tail == self.head then
 		return nil
 	end
-	local ins = self.store[self.tail]
-	self.store[self.tail] = nil
+	local ins = self[self.tail]
+	self[self.tail] = nil
 	self.tail = self.tail + 1
 	return ins
 end
 
 function list.size(self)
-	return self.head - self.tail;
+	return self.head - self.tail
 end
 
