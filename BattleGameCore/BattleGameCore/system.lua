4b5b8ec675d661e3f08263532dd0e8e4de5b70ac
lkstc112233 <296105011@qq.com>
2015-06-01 11:07:05 +0800

    E Card basics
    Added a useful function: LOAD_FOLDER
    fixed a bug in list.
    added 3 cards of E card.

--- BattleGameCore/BattleGameCore/system.lua
+++ BattleGameCore/BattleGameCore/system.lua
@@ -1,6 +1,7 @@
 system = {}
 
 dofile("list.lua")
+dofile("game.lua")
 
 system.isRunning = true
 system.messageList = list.new();
@@ -41,23 +42,32 @@ function system.getAnswer()
 end
 
 system.inited = false
+function system.init()
+	if not system.inited then
+		system.inited = true
+		system.messageList:inelm("s炸裂！超好玩的卡牌游戏！")
+		system.messageList:inelm("s游戏即将开始！")
+	end
+end
 
 system.counter = 0
 
 system.mainLoop = function()
-	if not system.inited then
-		system.inited = true
-		system.messageList:inelm("s你好，世界！")
-		system.messageList:inelm("a你的年龄是：")
-		system.messageList:inelm("a你的性别是？（1：男，2：女）")
-		system.messageList:inelm("A")
-	end
+	system.init()
 	if system.counter == 0 then
-		local ans = system.getAnswer()
-		if ans then
-			system.messageList:inelm(string.format('s你的年龄是：%d',ans))
-			system.counter = system.counter + 1
+		--local ans = system.getAnswer()
+		--if ans then
+		--	system.messageList:inelm(string.format('s你的年龄是：%d',ans))
+		--	system.counter = system.counter + 1
+		--end
+		system.messageList:inelm("s卡片共有" .. cards:size() .. "张")
+		local i = 1
+		while cards:size() ~= 0 do
+			local card = cards:outelm()
+			system.messageList:inelm("s第" .. i .. "张的名字是：" .. card)
+			system.messageList:inelm("s第" .. i .. "张是：" .. _G[card].getText())
 		end
+		system.counter = system.counter + 3
 	elseif system.counter == 1 then
 		local ans = system.getAnswer()
 		if ans then
